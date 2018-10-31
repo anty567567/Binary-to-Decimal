b4122ea2847ca8388dcd0cdfabe8aaa17f63721e
anty567567 <anty567567@hotmail.com>
2018-10-31 08:55:35 -0400

    First version

---
+++ Bin_to_Dec.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+int bin2dec(bool binary_string[]);
+
+int bin2dec(bool binary_string[]) {
+    int decimal{};
+    bool negative{}, one{};
+    //Checks if the binary is negative
+    if (binary_string[0] == 1) {
+        negative = true;
+    }
+
+    //Converts the binary from 2's complement to standard binary representation
+    for (int i = 15; i >= 0; --i) {
+        if (one) {
+            if (binary_string[i]) {
+                binary_string[i] = 0;
+            } else {
+                binary_string[i] = 1;
+            }
+        }
+        if (binary_string[i]){
+            one = true;
+        }
+    }
+    //Calculates the value of the binary in decimal
+    for (int i = 15; i > 0; --i) {
+        decimal += binary_string[i]*pow(2,15-i);
+    }
+    //Makes the decimal negative if the binary is negative
+    if (negative) {
+        decimal *= -1;
+    }
+    return decimal;
+}
+
+int main() {
+    bool binary[16] = {1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0};
+    std::cout << bin2dec(binary);
+    return 0;
+}
